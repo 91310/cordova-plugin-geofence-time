2941b7f73d0163dadab9a3fea8984c9521f3a4d5
91310 <32672842+91310@users.noreply.github.com>
2018-04-16 11:10:56 +0200

    correction pour Android 8 (Oreo)

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -1,6 +1,7 @@
 package com.cowbell.cordova.geofence;
 
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -25,14 +26,49 @@ public class GeoNotificationNotifier {
     }
 
     public void notify(Notification notification) {
+        logger.log(Log.DEBUG, "ON est bien dans la notification");
         notification.setContext(context);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-            .setVibrate(notification.getVibrate())
-            .setSmallIcon(notification.getSmallIcon())
-            .setLargeIcon(notification.getLargeIcon())
-            .setAutoCancel(true)
-            .setContentTitle(notification.getTitle())
-            .setContentText(notification.getText());
+        final int NOTIFY_ID = 1002;
+
+        // There are hardcoding only for show it's just strings
+        String name = "SmartRH_channel";
+        String id = "SmartRH_channel_1"; // The user-visible name of the channel.
+        String description = "SmartRH_first_channel"; // The user-visible description of the channel.
+
+        Intent intent;
+        PendingIntent pendingIntent;
+        NotificationCompat.Builder builder;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notificationManager.getNotificationChannel(id);
+            if (mChannel == null) {
+               mChannel = new NotificationChannel(id, name, importance);
+               mChannel.setDescription(description);
+               mChannel.enableVibration(true);
+               mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+               notificationManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(context, id);
+
+            builder.setContentTitle(notification.getTitle())  // required
+                   .setSmallIcon(notification.getSmallIcon()) // required
+                   .setContentText(notification.getText())  // required         
+                   .setAutoCancel(true)
+                   .setTicker(notification.getTitle())
+                   .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+      } else {
+
+           builder = new NotificationCompat.Builder(context);
+
+           builder.setContentTitle(notification.getTitle())                           // required
+                  .setSmallIcon(notification.getSmallIcon()) // required
+                  .setContentText(notification.getText())  // required
+                  .setAutoCancel(true)
+                  //.setContentIntent(pendingIntent)
+                  .setTicker(notification.getTitle())
+                  .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        } 
 
         if (notification.openAppOnClick) {
             String packageName = context.getPackageName();
@@ -41,13 +77,15 @@ public class GeoNotificationNotifier {
 
             if (notification.data != null) {
                 resultIntent.putExtra("geofence.notification.data", notification.getDataJson());
+                logger.log(Log.DEBUG, "Notif = " + notification.getDataJson());
+
             }
 
             TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
             stackBuilder.addNextIntent(resultIntent);
             PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
                 notification.id, PendingIntent.FLAG_UPDATE_CURRENT);
-            mBuilder.setContentIntent(resultPendingIntent);
+            builder.setContentIntent(resultPendingIntent);
         }
         try {
             Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -57,7 +95,8 @@ public class GeoNotificationNotifier {
         	beepHelper.startTone("beep_beep_beep");
             e.printStackTrace();
         }
-        notificationManager.notify(notification.id, mBuilder.build());
+        notificationManager.notify(NOTIFY_ID, builder.build());
+
         logger.log(Log.DEBUG, notification.toString());
     }
 }
